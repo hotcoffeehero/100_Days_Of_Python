cd6707be9d22e435c232c42511964bcc3acf62ac
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-01 07:09:45 +0900

    Day One: Band Name Generator

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+input("Hello. Let's name your dumb band. Press Enter.\n")
+
+city = input("What city did you grow up in?\n")
+
+petName = input("What is the name of your favorite pet?\n")
+
+print("Your new band name is " + city + " " + petName + ".\n")
