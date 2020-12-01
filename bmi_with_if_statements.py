aa2da4654a917418f2131a0eecfa827585dcb630
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-01 19:09:14 +0900

    Day 3: Multiple If Statements: Python Pizza

--- bmi_with_if_statements.py
+++ bmi_with_if_statements.py
@@ -5,12 +5,12 @@ weight = float(input("enter your weight in kg: "))
 BMI = weight / height ** 2
 
 if BMI < 18.5:
-  print("You are underweight.")
+  print(f"Your BMI is {BMI}. are underweight.")
 elif BMI < 25:
-  print("You are normal for your weight. ")
+  print(f"Your BMI is {BMI}. You are normal for your weight. ")
 elif BMI < 30:
-  print("You are slightly overweight. ")
+  print(f"Your BMI is {BMI}. You are slightly overweight. ")
 elif BMI < 35:
-  print("You are overweight. ")
+  print(f"Your BMI is {BMI}. You are overweight. ")
 else:
-  print("You are clinically obese. ")
+  print(f"Your BMI is {BMI}. You are clinically obese. ")
