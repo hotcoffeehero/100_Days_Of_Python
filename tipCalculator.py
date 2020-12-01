06a612a599dc4d36f7fb33c83404018b27cfa901
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-01 10:20:28 +0900

    Day Two: Tip Calculator

---
+++ tipCalculator.py
@@ -0,0 +1,5 @@
+bill = float(input("How much was your bill?"))
+tip = 1+ .01*float(input("How much would you like to tip? 10%, 12% or 15%"))
+people  = int(input("How many people are with you?"))
+total = round((bill/people)*tip, 2)
+print(f"Each person should pay ${total}")
