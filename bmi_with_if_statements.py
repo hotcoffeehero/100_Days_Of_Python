9b6434eaa6f640f52dab989dd4831b8bee0f829e
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-01 15:52:26 +0900

    BMI Calculator with if statements

---
+++ bmi_with_if_statements.py
@@ -0,0 +1,16 @@
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+BMI = weight/(height * height)
+
+if BMI <= 18.5:
+  print("You are underweight")
+elif BMI <= 25:
+  print("You are normal for your weight")
+elif BMI <= 30:
+  print("You are slightly overweight")
+elif BMI <= 35:
+  print("You are overweight")
+else:
+  print("You are clinically obese")
--- tipCalculator.py
+++ tipCalculator.py
@@ -1,4 +1,4 @@
-bill = float(input("How much was your bill?"))
+bill = float(input("How much was your bill? $"))
 tip = 1+ .01*float(input("How much would you like to tip? 10%, 12% or 15%"))
 people  = int(input("How many people are with you?"))
 total = round((bill/people)*tip, 2)
