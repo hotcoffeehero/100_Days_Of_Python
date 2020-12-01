50375c57746c510a385265b885645e48391ccdb6
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-01 16:00:20 +0900

    BMI Calculator Updated

--- bmi_with_if_statements.py
+++ bmi_with_if_statements.py
@@ -2,15 +2,15 @@
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 
-BMI = weight/(height * height)
+BMI = weight / height ** 2
 
-if BMI <= 18.5:
-  print("You are underweight")
-elif BMI <= 25:
-  print("You are normal for your weight")
-elif BMI <= 30:
-  print("You are slightly overweight")
-elif BMI <= 35:
-  print("You are overweight")
+if BMI < 18.5:
+  print("You are underweight.")
+elif BMI < 25:
+  print("You are normal for your weight. ")
+elif BMI < 30:
+  print("You are slightly overweight. ")
+elif BMI < 35:
+  print("You are overweight. ")
 else:
-  print("You are clinically obese")
+  print("You are clinically obese. ")
