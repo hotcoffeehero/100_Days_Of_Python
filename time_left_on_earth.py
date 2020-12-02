4b5998798237c4ffba44efa08f15b6144c306056
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-02 20:55:17 +0900

    Day 2: Time Left on Earth

---
+++ time_left_on_earth.py
@@ -0,0 +1,15 @@
+
+age = int(input("What is your current age?"))
+
+
+yearsLeft = 90 - age
+weeksLeft = yearsLeft * 52
+daysLeft = yearsLeft * 365
+hoursLeft = daysLeft * 24
+
+
+print(f"You have {weeksLeft} weeks left on Earth. Enjoy.")
+
+print(f"You have {daysLeft} days left on Earth. Enjoy.")
+
+print(f"You have {hoursLeft} hours left on Earth. Enjoy.")
