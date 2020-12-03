b3b8287e8756c1689a2354a93f78a1b4e1014a7f
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-03 21:57:40 +0900

    Day 4: Heads or Tells - Revise and Working

--- 04_heads_or_tails.py
+++ 04_heads_or_tails.py
@@ -1,22 +1,57 @@
 import random
 
-guess = input("Heads or Tails?\n").lower()
+guess = input("Heads or Tails?").lower()
+
+if guess == 'heads' or guess == 'tails':
+
+  coinNumber = random.randint(0, 1)
+
+  if coinNumber == 0:
+    coinFace = 'heads'
+  else:
+    coinFace = 'tails'
 
 
-if guess == 'heads':
-  guessStr = 'heads'
-  guessInt = 0
+  if coinNumber == 0 and guess == 'heads':
+    print(f"You win! Dice said {coinFace}")
+
+  elif coinNumber == 0 and guess == 'tails':
+    print(f"You Lose. Dice said {coinFace}")
+
+  elif coinNumber == 1 and guess == 'tails':
+    print(f"You win! Dice said {coinFace}")
+
+  else:
+    print(f"You Lose. Dice said {coinFace}")
+
 else:
-  guessStr = 'tails'
-  guessInt = 1
+  print(f"You said '{guess}'. Please hit 'Run' and try again.")
 
-print(f"You said {guess}!")
+# ==================================================================
+# HERE IS WHERE I WENT WRONG AT THE END OF 12/02/202
 
-print(input("Flip a coin!\n"))
+# import random
 
-ran = random.randint(0,1)
+# guess = input("Heads or Tails?\n").lower()
 
-if ran == guessInt:
-  print(f"The coin said {guessStr}! You win!")
-else:
-  print(f"The coin said {guessStr}! You lose!")
+# if guess == 'heads' or guess == 'tails':
+#   if guess == 'heads':
+#     guessStr = 'heads'
+#     guessInt = 0
+#   else:
+#     guessStr = 'tails'
+#     guessInt = 1
+
+#     print(f"You said {guess}!")
+
+#     print(input("Flip a coin! (Press Enter, lol.)\n"))
+
+#     ran = random.randint(0,1)
+
+#     if ran == guessInt:
+#         print(f"The coin said {guessStr}! You win!")
+#     else:
+#         print(f"The coin said {guessStr}! You lose!")
+
+# else:
+#   print(f"You said {guess}. Click Run and try again. Goodbye. Please take this seriously next time.")
