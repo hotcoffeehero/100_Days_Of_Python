0be35affdb56981655849e8fe290a12f6cc4c1c1
hotcoffeehero <hotcoffeehero@gmail.com>
2021-03-26 18:52:44 +0900

    Day 25: CSVs

--- Day_25 - CSV/main.py
+++ Day_25 - CSV/main.py
@@ -10,4 +10,15 @@ import csv
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data['temp'])
+# # print(data['temp'])
+#
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data['temp'].to_list()
+# print(temp_list)
+#
+# average = sum(temp_list) / len(temp_list)
+# print(average )
+
+print(data[data.day == 'Monday'])
